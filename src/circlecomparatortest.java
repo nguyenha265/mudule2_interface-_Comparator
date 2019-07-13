db02452087a438f34da97c1f0199a3b00e1508f8
nguyenha265 <nguyenha2651988@gmail.com>
2019-07-13 18:06:18 +0700

    thuc hanh

---
+++ src/circlecomparatortest.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class circlecomparatortest {
+        public static void main(String[] args) {
+            Circle[] circles = new Circle[3];
+            circles[0] = new Circle(3.6f);
+            circles[1] = new Circle();
+            circles[2] = new Circle(3.5, "indigo", false);
+
+            System.out.println("Pre-sorted:");
+            for (Circle circle : circles) {
+                System.out.println(circle.area());
+            }
+            Comparator circleComparator = new CircleComparator();
+            Arrays.sort(circles, circleComparator);
+
+            System.out.println("After-sorted:");
+            for (Circle circle : circles) {
+                System.out.println(circle.area());
+            }
+        }
+    }
+
+
